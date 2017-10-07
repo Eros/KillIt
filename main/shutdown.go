afb756e841f9bb8e1b30f1d06ecdc0139459308c
Eros <george.marr.cobc@gmail.com>
2017-10-07 23:29:28 +0100

    fuck

--- main/shutdown.go
+++ main/shutdown.go
@@ -10,6 +10,7 @@ import (
 
 	"sync"
 	"os/exec"
+	"log"
 )
 
 type commandList struct {
@@ -21,4 +22,26 @@ func (c1 * commandList) Add(cmd *exec.Cmd){
 	c1.Lock()
 	c1.commands = append(c1.commands, cmd)
 	c1.Unlock()
+}
+
+func (c1 * commandList) KillAll(){
+	for _, c := range c1.commands{
+		if c.Process != nil {
+			_ = c.Process.Kill()
+		}
+	}
+	c1.commands = nil
+}
+
+func parallelExecute(cmd *exec.Cmd, wg *sync.WaitGroup){
+	err := cmd.Start()
+	if err != nil {
+		log.Printf("Error starting pc %s: %s", cmd.Path, err)
+	}
+	wg.Add(1)
+	err = cmd.Wait()
+	wg.Done()
+	if err != nil {
+		log.Printf("Error starting pc %s: %s", cmd.Path, err)
+	}
 }
